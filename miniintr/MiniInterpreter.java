5a50af09c5a3a8d1a21ab0df746b3e55967eda0b
Kondo Takeo <takeoverjp@gmail.com>
2019-03-27 12:15:49 +0900

    Support single number.

--- miniintr/MiniInterpreter.java
+++ miniintr/MiniInterpreter.java
@@ -6,7 +6,7 @@ public class MiniInterpreter {
     }
 
     private int eval(String expression) {
-        return 0;
+        return expression.charAt(0) - '0';
     }
 
     public static void main(String[] args) {
